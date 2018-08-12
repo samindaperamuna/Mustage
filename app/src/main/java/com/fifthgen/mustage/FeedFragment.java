6c35e9a607d2dab99a21043b2b117bb9eb6b244c
Saminda Peramuna <saminda_lk@msn.com>
2018-08-12 20:39:39 +0530

    Implemented Binocular.

--- app/src/main/java/com/fifthgen/mustage/FeedFragment.java
+++ app/src/main/java/com/fifthgen/mustage/FeedFragment.java
@@ -1,11 +1,14 @@
 package com.fifthgen.mustage;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.annotation.SuppressLint;
 import android.app.Fragment;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -13,8 +16,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.Toolbar;
 
+import com.fifthgen.mustage.adapters.MomentsAdapter;
 import com.fifthgen.mustage.adapters.PaginationAdapter;
 import com.fifthgen.mustage.adapters.StoryViewHolder;
 import com.fifthgen.mustage.model.Story;
@@ -23,15 +28,34 @@ import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
 import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
-public class FeedFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FeedFragment extends Fragment implements ValueEventListener, View.OnClickListener {
 
     public static final String EXTRA_STORY_ID = "feed.storyId";
     private RecyclerView mList;
     private RecyclerView.Adapter mAdapter;
     private View emptyView;
+
+    // Binocular definitions.
+
+    // Users followings database reference.
+    private DatabaseReference mUserRef;
+    private List<DatabaseReference> mFollowingRefs;
+    private List<User> users;
+    private RecyclerView momentsRecycler;
+    private ImageButton binocularButton;
+
     private RecyclerView.AdapterDataObserver emptyObserver = new RecyclerView.AdapterDataObserver() {
         @Override
         public void onItemRangeInserted(int positionStart, int itemCount) {
@@ -74,6 +98,18 @@ public class FeedFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_feed, container, false);
 
+        // Binocular init start.
+        ImageButton binocularButton = view.findViewById(R.id.binocularButton);
+        binocularButton.setOnClickListener(this);
+
+        momentsRecycler = view.findViewById(R.id.momentsRecycler);
+        RecyclerView.LayoutManager binocularLayoutMan = new LinearLayoutManager(getActivity());
+        momentsRecycler.setLayoutManager(binocularLayoutMan);
+        momentsRecycler.setItemAnimator(new DefaultItemAnimator());
+        users = new ArrayList<>();
+
+        // Binocular init end.
+
         mList = view.findViewById(R.id.list);
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getActivity());
         mList.setLayoutManager(mLayoutManager);
@@ -100,7 +136,6 @@ public class FeedFragment extends Fragment {
             showFeed();
         }
 
-
         String versionName = " ";
         try {
             PackageInfo packageInfo = getActivity().getPackageManager().getPackageInfo(getActivity().getPackageName(), 0);
@@ -116,6 +151,10 @@ public class FeedFragment extends Fragment {
             switch (menuItem.getItemId()) {
                 case R.id.action_settings:
                     startActivity(new Intent(getActivity(), SettingsActivity.class));
+                    break;
+                case R.id.action_binocular:
+                    // Show binocular activity.
+                    startActivity(new Intent(getActivity(), BinocularActivity.class));
                     break;
             }
 
@@ -191,6 +230,111 @@ public class FeedFragment extends Fragment {
                 emptyView.setVisibility(View.GONE);
                 mList.setVisibility(View.VISIBLE);
             }
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        mUserRef = User.following(User.currentKey());
+        mUserRef.addValueEventListener(this);
+    }
+
+    @Override
+    public void onStop() {
+        if (mUserRef != null) {
+            mUserRef.removeEventListener(this);
+            mUserRef = null;
+        }
+
+        if (mFollowingRefs != null) {
+            for (DatabaseReference ref : mFollowingRefs) {
+                ref.removeEventListener(this);
+            }
+
+            mFollowingRefs = null;
+        }
+
+        super.onStop();
+    }
+
+    private void loadBinocular() {
+        if (users != null && !users.isEmpty()) {
+            MomentsAdapter adapter = new MomentsAdapter(R.layout.moments_item, users);
+            momentsRecycler.setHasFixedSize(true);
+            momentsRecycler.setAdapter(adapter);
+        }
+    }
+
+    @Override
+    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+        if (dataSnapshot.exists()) {
+            DatabaseReference ref = dataSnapshot.getRef();
+
+            if (mUserRef != null && ref.equals(mUserRef)) {
+                // Get users followings and load their details.
+                GenericTypeIndicator<HashMap<String, Boolean>> userType = new GenericTypeIndicator<HashMap<String, Boolean>>() {
+                };
+                HashMap<String, Boolean> followings = dataSnapshot.getValue(userType);
+
+                if (followings != null && !followings.isEmpty()) {
+                    mFollowingRefs = new ArrayList<>();
+
+                    for (Map.Entry<String, Boolean> entry : followings.entrySet()) {
+                        if (entry.getValue()) {
+                            DatabaseReference userRef = User.collection(entry.getKey());
+                            userRef.addValueEventListener(this);
+                            mFollowingRefs.add(userRef);
+                        }
+                    }
+                }
+            } else if (mFollowingRefs != null && !mFollowingRefs.isEmpty()) {
+
+                for (DatabaseReference userRef : mFollowingRefs) {
+                    if (ref.equals(userRef)) {
+                        User user = dataSnapshot.getValue(User.class);
+                        users.remove(user);
+                        users.add(user);
+                    }
+                }
+
+                // Load binocular view.
+                loadBinocular();
+            }
+        }
+    }
+
+    @Override
+    public void onCancelled(@NonNull DatabaseError databaseError) {
+        Log.e("Fetch Moments", "An error occurred: " + databaseError.getMessage());
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.binocularButton:
+                // Animate Binocular into view.
+                if (momentsRecycler.getVisibility() == View.VISIBLE) {
+                    momentsRecycler.animate()
+                            .translationY(0)
+                            .alpha(0.0f)
+                            .setListener(new AnimatorListenerAdapter() {
+                                @Override
+                                public void onAnimationEnd(Animator animation) {
+                                    super.onAnimationEnd(animation);
+                                    momentsRecycler.setVisibility(View.GONE);
+                                }
+                            });
+                } else {
+                    momentsRecycler.setVisibility(View.VISIBLE);
+                    momentsRecycler.setAlpha(0.0f);
+                    momentsRecycler.animate()
+                            .translationY(momentsRecycler.getHeight())
+                            .alpha(1.0f)
+                            .setListener(null);
+                }
+                break;
         }
     }
 }
---
+++ chips-input-layout/src/main/java/com/fifthgen/chips/FilterableRecyclerView.java
@@ -0,0 +1,120 @@
+package com.fifthgen.chips;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.Rect;
+import android.support.v4.view.ViewCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.widget.Filter;
+import android.widget.Filterable;
+
+/**
+ * Copyright © 2017 Tyler Suehr
+ *
+ * Subclass of {@link RecyclerView} to enable calling to {@link Filter} object
+ * to filter the appropriate data source, and then receive a callback once the
+ * filtering is completed.
+ *
+ * Callbacks for filtering events are necessary because they are used to show/hide
+ * this view respectively.
+ *
+ * @author Tyler Suehr
+ * @version 1.0
+ */
+class FilterableRecyclerView extends RecyclerView implements ChipComponent {
+    /* Used to find its location in window */
+    private ChipsInputLayout mChipsInput;
+    /* Used to trigger filtering and receive callbacks to show or hide this */
+    private Filter mFilter;
+
+
+    FilterableRecyclerView(Context c) {
+        super(c);
+        setBackgroundColor(Color.WHITE);
+        setLayoutManager(new LinearLayoutManager(c));
+        setVisibility(GONE);
+    }
+
+    @Override
+    public void setChipOptions(ChipOptions options) {
+        ViewCompat.setElevation(this, options.mFilterableListElevation);
+        if (options.mFilterableListBackgroundColor != null) {
+            getBackground().setColorFilter(options.mFilterableListBackgroundColor
+                    .getDefaultColor(), PorterDuff.Mode.SRC_ATOP);
+        }
+    }
+
+    <T extends RecyclerView.Adapter & Filterable>
+    void setup(T adapter, ChipsInputLayout chipsInputLayout) {
+        setAdapter(adapter);
+        mFilter = adapter.getFilter();
+        mChipsInput = chipsInputLayout;
+    }
+
+    /**
+     * Applies the given filter pattern to the filter. If the filter yields no results,
+     * then we hide this filterable recycler, or show it otherwise.
+     *
+     * @param filter Filter pattern
+     */
+    void filterChips(CharSequence filter) {
+        if (filter != null) {
+            mFilter.filter(filter, new Filter.FilterListener() {
+                @Override
+                public void onFilterComplete(int count) {
+                    // Show if, and only if, there are results
+                    if (count > 0) {
+                        fadeIn();
+                    } else {
+                        fadeOut();
+                    }
+                }
+            });
+        }
+    }
+
+    /**
+     * Uses alpha animation to fade in the current view if it's not visible.
+     */
+    void fadeIn() {
+        if (getVisibility() == VISIBLE) { return; }
+
+        // Get visible window (keyboard shown)
+        Rect r = new Rect();
+        final View rootView = getRootView();
+        rootView.getWindowVisibleDisplayFrame(r);
+
+        int[] coord = new int[2];
+        mChipsInput.getLocationInWindow(coord);
+
+        ViewGroup.MarginLayoutParams lp = (MarginLayoutParams)getLayoutParams();
+        lp.topMargin = coord[1] + mChipsInput.getHeight();
+
+        // Height of the keyboard
+        lp.bottomMargin = rootView.getHeight() - r.bottom;
+        setLayoutParams(lp);
+
+        AlphaAnimation anim = new AlphaAnimation(0.0f, 1.0f);
+        anim.setDuration(200);
+        startAnimation(anim);
+
+        setVisibility(VISIBLE);
+    }
+
+    /**
+     * Uses alpha animation to fade out the current view if it's not gone.
+     */
+    void fadeOut() {
+        if (getVisibility() == GONE) { return; }
+
+        AlphaAnimation anim = new AlphaAnimation(1.0f, 0.0f);
+        anim.setDuration(200);
+        startAnimation(anim);
+
+        setVisibility(GONE);
+    }
+}
